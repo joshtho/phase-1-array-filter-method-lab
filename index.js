3a7ee6e175edff338eecbc20b875df762661cc56
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-10 22:16:18 -0600

    commit tree 33780b3e21fc915284a6da6da461de5d2ebe5480

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+   let answer = array.filter(obj => obj.toLowerCase() === string.toLowerCase());
+    if (answer) {
+        return answer;
+    }
+}
+function fuzzyMatch(array, string) {
+    let answer = array.filter(obj => obj.startsWith(string))
+    if (answer) {
+        return answer;
+    }
+}
+function matchName(array, string) {
+    let answer = array.filter(obj => obj.name === string)
+    if (answer) {
+        return answer;
+    }
+}
